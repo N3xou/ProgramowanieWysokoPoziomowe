2afe9b36fc985216d2745c83e9925e85b892c95a
Yami <santox25@gmail.com>
2024-10-01 13:14:40 +0200

    Init + Zad1

---
+++ main.py
@@ -0,0 +1,9 @@
+# Zadanie 1
+a,b,c,d = 2,5,5.0,4.2
+print('a=',a,'b=',b,'c=',c,'d=',d)
+print('a + b:',a + b,'\na + c:', a + c,'\na + d:', a + d)
+print('a * b:',a * b,'\na * c:', a * c,'\na * d:', a * d)
+
+# inty zmieniaja sie na floaty
+
+# Zadanie 2
