7adc7ad6ed7629a9eeaa2f9ba043452c0601284a
Yami <santox25@gmail.com>
2024-10-01 14:04:00 +0200

    Zad 9-12

--- main.py
+++ main.py
@@ -61,8 +61,8 @@ else:
     print('Nieparzysta')
 
 # Zadanie 8
-
-rok  = int(input('Rok: '))
+rok = 5
+#rok  = int(input('Rok: '))
 
 if rok % 400 == 0:
     print('Przestepny')
@@ -72,3 +72,44 @@ elif rok % 4 == 0:
     print('Przestepny')
 else:
     print('Nie Przestepny')
+
+# Zadanie 9
+
+f = 93.7415
+#f = float(input('f: '))
+print(int(f))
+print(round(f%1,4))
+
+# Zadanie 10
+
+f,g = 2.314,65.45
+#f = float(input('f: '))
+#g = float(input('g: '))
+print(round(int(f) + g%1, 4))
+print(round(int(g) + f%1, 4))
+
+# Zadanie 11
+a,b = 3,2
+
+if a**b == b**a:
+    print('Oba potęgowania są równe: ', a**b)
+elif a**b > b**a:
+    print(f"{a} do potęgi {b} = {a ** b}, większe od {b} do potęgi {a} = {b ** a}")
+
+else:
+    print(f"{b} do potęgi {a} = {b ** a}, większe od {a} do potęgi {b} = {a ** b}")
+
+# Zadanie 12
+a,b,c = 2**(1/2),3**(1/3), 5**(1/5)
+if a > b and a > c:
+    print(f'Największa to a: {a}')
+elif b > a and b > c:
+    print(f'Największa to b: {b}')
+else:
+    print(f'Największa to c: {c}')
+if a < b and a < c:
+    print(f'Najmniejsza to a: {a}')
+elif b < a and b < c:
+    print(f'Najmniejsza to b: {b}')
+else:
+    print(f'Najmniejsza to c: {c}')
