42e069cded650c6de3a976fb0d6749f6dc1194d4
Yami <santox25@gmail.com>
2024-10-01 13:40:39 +0200

    Zad 5-8

--- main.py
+++ main.py
@@ -22,11 +22,53 @@ print (s + t, t + s + t, 2 * s)
 # wartosci traktowane sa jako string nawet przy wpisaniu numerycznych
 
 # Zadanie 4
-i=int(input('Podaj i'))
-j=int(input('Podaj j'))
-print(i,j)
-print(type(i))
-print(i+j)
-print(type(i+j))
+#i=int(input('Podaj i'))
+#j=int(input('Podaj j'))
+#print(i,j)
+#print(type(i))
+#print(i+j)
+#print(type(i+j))
 
 # mozemy wprowadzic tylko int, inaczej jest blad. inty dodaja sie normalnie(jak liczby)
+
+# Zadanie 5
+
+a,b = 2,3
+print(a%b)
+b = 4
+print(a%b)
+b = 10
+print(a%b)
+a,b = 5,3
+print(a%b)
+a = 6
+print(a%b)
+# % jest reszta z dzielenia
+
+# Zadanie 6
+#a,b,c = int(input('a: ')), int(input('b: ')), int(input('c: '))
+g  = [a,b,c]
+for l in g:
+    if l > 10:
+        print(l)
+
+# Zadanie 7
+
+#a = int(input('a: '))
+if a % 2 == 0:
+    print('Parzysta')
+else:
+    print('Nieparzysta')
+
+# Zadanie 8
+
+rok  = int(input('Rok: '))
+
+if rok % 400 == 0:
+    print('Przestepny')
+elif rok % 100 == 0:
+    print('Nie Przestepny')
+elif rok % 4 == 0:
+    print('Przestepny')
+else:
+    print('Nie Przestepny')
