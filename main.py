a3a310773ad6d0585c9f6dba2e81235fda6df623
Yami <santox25@gmail.com>
2024-10-01 13:21:26 +0200

    Zad 1-4

--- main.py
+++ main.py
@@ -7,3 +7,26 @@ print('a * b:',a * b,'\na * c:', a * c,'\na * d:', a * d)
 # inty zmieniaja sie na floaty
 
 # Zadanie 2
+s = 'pies'
+t = 'kot'
+print (s + t, t + s + t, 2 * s)
+
+# Zadanie 3
+#i=input('Podaj i')
+#j=input('Podaj j')
+#print(i,j)
+#print(type(i))
+#print(i+j)
+#print(type(i+j))
+
+# wartosci traktowane sa jako string nawet przy wpisaniu numerycznych
+
+# Zadanie 4
+i=int(input('Podaj i'))
+j=int(input('Podaj j'))
+print(i,j)
+print(type(i))
+print(i+j)
+print(type(i+j))
+
+# mozemy wprowadzic tylko int, inaczej jest blad. inty dodaja sie normalnie(jak liczby)
