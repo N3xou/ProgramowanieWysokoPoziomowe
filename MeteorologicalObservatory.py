c7c9399f54a23f98c9a0a891a723ed48546ee129
Yami <santox25@gmail.com>
2024-10-24 00:21:16 +0200

    add_measurement get_measurement_by_location get_average_temperature

--- MeteorologicalObservatory.py
+++ MeteorologicalObservatory.py
@@ -12,3 +12,18 @@ class WeatherData:
     def __str__(self):
         return '\n'.join(str(measurement) for measurement in self.measurements)
 
+    def add_measurement(self, location: str, datetime: str, value: float):
+        measurement = Measurement(location, datetime, value)
+        self.measurements.append(measurement)
+
+    def get_measurements_by_location(self, location: str):
+        return [measurement for measurement in self.measurements if measurement.location == location]
+
+    def get_average_temperature(self, location: str):
+        location_measurements = self.get_measurements_by_location(location)
+        if not location_measurements:
+            return None
+
+        total_value = sum(measurement.value for measurement in location_measurements)
+        return total_value / len(location_measurements)
+
