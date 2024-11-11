98813e2cef9e1b5628fb37b26d99ad315ad5c633
Yami <santox25@gmail.com>
2024-11-12 00:56:43 +0100

    v1

--- PriceMonitoring.py
+++ PriceMonitoring.py
@@ -1,25 +1,51 @@
 import requests
 from bs4 import BeautifulSoup
 import csv
+import re
 class Product:
-    def __init__(self, name, url, current_price = 0):
+    def __init__(self, name, url, current_price = 0.0):
         self.name = name
         self.url = url
         self.current_price = current_price
     def update_price(self):
         response = requests.get(self.url)
         soup = BeautifulSoup(response.text, 'html.parser')
-        price_element = soup.find('span', {'class': 'product-price'})
-        if 'PLN' in price_element.text:
-            price = float(price_element.text.replace(' PLN', '').replace(',', '.'))
-        elif 'USD' or '$' in price_element.text:
-            price = float(price_element.text.replace(' USD', '').replace(',', '.'))
-            price = price * 4
-        elif 'EUR' or '€' in price_element.text:
-            price = float(price_element.text.replace(' EUR', '').replace(',', '.'))
-            price = price * 4.4
+        price_element = soup.find('p', class_='price_color')
+        if price_element:
+            # Extract the price text and strip out any non-numeric characters
+            price_text = price_element.text.strip()
 
-        pass
+
+            price_number = re.sub(r'[^\d.,]', '', price_text)  # Regex
+
+            price_number = price_number.replace(',', '.')
+
+            try:
+                price = float(price_number)
+                if '£' in price_text:
+                    price *= 5
+                elif 'PLN' in price_text:
+                    pass
+                elif 'USD' in price_text or '$' in price_text:
+                    price *= 4
+                elif 'EUR' in price_text or '€' in price_text:
+                    price *= 4.4
+                else:
+                    print('Nie rozpoznano waluty')
+                    price = 0
+
+                self.current_price = price
+
+                # For debugging: print the extracted price text
+                print(f"Zaktualizowano cene produktu {self.name } do: {price} PLN")
+
+            except ValueError:
+                print(f"Wystąpił błąd w rozpoznaniu ceny: {price_text}")
+        else:
+            print("Nie odnaleziono ceny.")
+
+
+
 class PriceMonitor:
     def __init__(self, products = []):
         self.products = products
@@ -27,6 +53,7 @@ class PriceMonitor:
         product = Product(name,url)
         product.update_price()
         self.products.append(product)
+        print('Pomyślnie dodano produkt.')
     def remove_product(self, name):
         matching_products = [product for product in self.products if name.lower() in product.name.lower()]
 
@@ -40,18 +67,21 @@ class PriceMonitor:
         try:
             choice = int(input("Wybierz numer produktu do usunięcia lub wprowadz 0 aby anyluwać.: "))
             if choice == 0:
-                print("Operation cancelled.")
+                print("Anulowanie.")
                 return
             elif 1 <= choice <= len(matching_products):
                 self.products.remove(matching_products[choice - 1])
-                print(f"{matching_products[choice - 1].name} has been removed.")
+                print(f"{matching_products[choice - 1].name} został usunięty.")
             else:
-                print("Invalid selection.")
+                print("Wybrano niepoprawną opcję.")
         except ValueError:
-            print("Invalid input. Operation cancelled.")
+            print("Błąd wprowadzono inny znak niż cyfra.")
     def update_all_prices(self):
-        for product in self.products:
-            product.update_price()
+        if len(self.products) < 1:
+            print('Nie odnaleziono produktów do zaktualizowania cen.')
+        else:
+            for product in self.products:
+                product.update_price()
 
     def get_cheapest_product(self):
         if not self.products:
@@ -59,10 +89,10 @@ class PriceMonitor:
             return None
 
         cheapest_product = min(self.products, key=lambda product: product.current_price)
-        print(f"The cheapest product is {cheapest_product.name} with a price of {cheapest_product.current_price} PLN")
+        print(f"Najtańszy produkt to {cheapest_product.name} kosztujący {cheapest_product.current_price} PLN")
         return cheapest_product
     def save_prices(self, filename):
-        with open('ceny.csv', 'w', newline='') as csvfile:
+        with open(filename, 'w', newline='') as csvfile:
             writer = csv.writer(csvfile)
             for product in self.products:
                 writer.writerow([product.name, product.current_price])
@@ -73,23 +103,42 @@ class PriceMonitor:
             #self.products = []   # clearing products
             for row in reader:
                 name, price = row
+                print(name)
+                print(price)
                 product = Product(name, float(price))
                 self.products.append(product)
 
+    def display_all_products(self):
+        if not self.products:
+            print("Brak produktów na liście.")
+        else:
+            print("Lista produktów i ich cen:")
+            for product in self.products:
+                print(f"Produkt: {product.name}, Cena: {product.current_price}")
 
 def main():
     monitor = PriceMonitor()
+    #product1 = Product("A Light in the Attic",
+     #                  "http://books.toscrape.com/catalogue/a-light-in-the-attic_1000/index.html")
+    #product2 = Product("Tipping the Velvet", "http://books.toscrape.com/catalogue/tipping-the-velvet_999/index.html")
+    #product3 = Product("Soumission", "http://books.toscrape.com/catalogue/soumission_998/index.html")
+
+    #monitor = PriceMonitor(products=[product1, product2, product3])
+
+    # Update prices for the initialized products
+    monitor.update_all_prices()
+
     print("Witaj w systemie monitorowania cen!")
 
     while True:
         print("\n1. Dodaj produkt do monitorowania")
         print("2. Usuń produkt")
         print("3. Zaktualizuj ceny produktów")
-        print("4. Wyświetl listę produktów i ich ceny")
-        print("5. Znajdź najtańszy produkt")
-        print("6. Zapisz ceny do pliku")
-        print("7. Wczytaj ceny z pliku")
-        print("8. Wyjdź")
+        print("4. Znajdź najtańszy produkt")
+        print("5. Zapisz ceny do pliku")
+        print("6. Wczytaj ceny z pliku")
+        print("7. Wyjdź")
+        print("8. Wyświetl wszystkie produkty")
 
         choice = input("Wybierz opcję: ")
 
@@ -103,18 +152,18 @@ def main():
         elif choice == "3":
             monitor.update_all_prices()
         elif choice == "4":
-            monitor.display_products()
-        elif choice == "5":
             monitor.get_cheapest_product()
-        elif choice == "6":
+        elif choice == "5":
             filename = input("Podaj nazwę pliku do zapisania cen (np. ceny.csv): ")
             monitor.save_prices(filename)
-        elif choice == "7":
+        elif choice == "6":
             filename = input("Podaj nazwę pliku do wczytania cen (np. ceny.csv): ")
             monitor.load_prices(filename)
-        elif choice == "8":
+        elif choice == "7":
             print("Zamykanie programu.")
             break
+        elif choice == "8":
+            monitor.display_all_products()
         else:
             print("Nieprawidłowy wybór. Spróbuj ponownie.")
 
